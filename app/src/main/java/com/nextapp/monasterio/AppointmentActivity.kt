6aaf24d1e5dd09857b093ab7dc6d4b8796094ddb
belmeji <belidmejialargo@gmail.com>
2025-10-24 00:48:33 +0200

    Cambios en los imports para que funcionen con nextapp

--- app/src/main/java/com/nextapp/monasterio/AppointmentActivity.kt
+++ app/src/main/java/com/nextapp/monasterio/AppointmentActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.smov_monasterio.ui.theme.Smov_monasterioTheme
+import com.nextapp.monasterio.ui.theme.Smov_monasterioTheme
 
 class AppointmentActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
